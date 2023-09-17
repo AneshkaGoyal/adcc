b2ad0157b4e72dd25b374c414af3aa0724a11524
Aneshka Goyal <Aneshka.Goyal@amexgbt.com>
2023-09-17 14:20:42 +0530

    deploy setup

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const path = require("path");
 
 // server used to send send emails
 const app = express();
+app.use(express.static(path.resolve(__dirname, '../build')));
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
@@ -45,4 +47,8 @@ router.post("/contact", (req, res) => {
             res.json({ code: 200, status: "Message Sent" });
         }
     });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
