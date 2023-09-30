72e61295dfed024bc284d5e82bc398ff36284641
AneshkaGoyal <36636940+AneshkaGoyal@users.noreply.github.com>
2023-09-30 15:14:39 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -42,7 +42,7 @@ function Contact(){
         e.preventDefault();
         if(isVerified()) {
             setButtonText("Sending...");
-            let response = await fetch("http://localhost:5000/contact", {
+            let response = await fetch("/contact", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json;charset=utf-8",
