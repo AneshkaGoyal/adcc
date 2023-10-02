0979247570c78baa22aaf45c94eca9e1ee766760
AneshkaGoyal <36636940+AneshkaGoyal@users.noreply.github.com>
2023-10-02 15:37:17 +0530

    Update SuccessMetrics.jsx

--- src/components/SuccessMetrics.jsx
+++ src/components/SuccessMetrics.jsx
@@ -2,6 +2,14 @@ import Carousel from "react-multi-carousel"
 import "react-multi-carousel/lib/styles.css";
 import {Container,Row,Col} from "react-bootstrap";
 import dummyImg from "../assets/img/dummyClinicImg.jpeg"
+import patientTreatment from "../assets/img/patientTreatment.jpeg"
+import clinicChair from "../assets/img/ClinicChair.jpeg"
+import clinicBoard from "../assets/img/ClinicBoard.jpeg"
+import clinicBoardTwo from "../assets/img/ClinicBoardTwo.jpeg"
+import clinicWaiting from "../assets/img/ClinicWaitingImg1.jpeg"
+import clinicWaiting2 from "../assets/img/ClinicWaitingImg2.jpeg"
+import clinicWaiting3 from "../assets/img/ClinicWaiting3.jpeg"
+
 
 function SuccessMetrics(){
 
@@ -34,20 +42,38 @@ function SuccessMetrics(){
                             <h2>
                                 Meet Us
                             </h2>
-                            <p>One line or two about the docs and clinic. One line or two about the docs and clinic. One line or two about the docs and clinic. One line or two about the docs and clinic.</p>
+                            <p>Aneshka Dental Care Centre is a premier dental clinic in sector 10 A Gurugram, since last 25+ years.
+                               <br/>Dr.Ritu Goyal, a passionate professional dentist who holds an experience of more than 25 years. She has an expertise
+                               in all dental procedures and has a line up of happy patients.<br/>
+                               Dr.(Major) Y.K Goyal, with an additional experience of Indian Defence Services as dental surgeon has 25 plus
+                               years of professional experience also provides consulting dental services at other clinics.<br/>
+                               Our dental clinic offers state-of-the-art dental treatment meeting international standards and follows all the
+                               sterlisation protocols. We use modern equipped hightech dental chairs. We have in depth knowlege to provide you perfect smile.
+                               Ours is one of the best dental clinics in Gurugram than provides you a wide range of dental services to meet all your dental needs under one roof.
+                            </p>
                             <Carousel responsive={responsive} infinite={true} className="skill-slider" autoPlay={true}>
                                 <div className="item">
-                                    <img src={dummyImg} alt="Images"/>
-                                    <h5>Clinic</h5>
+                                    <img src={clinicChair} alt="Images"/>
                                 </div>
                                 <div className="item">
-                                    <img src={dummyImg} alt="Images"/>
+                                    <img src={clinicBoard} alt="Images"/>
+                                </div>
+                                <div className="item">
+                                    <img src={clinicBoardTwo} alt="Images"/>
+                                </div>
+                                <div className="item">
+                                    <img src={patientTreatment} alt="Images"/>
+                                </div>
+                                <div className="item">
+                                    <img src={clinicWaiting} alt="Images"/>
                                     <h5>Waiting</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={dummyImg} alt="Images"/>
-                                    <h5>Applicance</h5>
+                                    <img src={clinicWaiting2} alt="Images"/>
                                 </div>
+                                {/*<div className="item">*/}
+                                {/*    <img src={clinicWaiting3} alt="Images"/>*/}
+                                {/*</div>*/}
                             </Carousel>
                         </div>
                     </Col>
