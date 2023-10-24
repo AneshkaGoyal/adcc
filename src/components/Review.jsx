3cb164e64d83ff5d42ebd449a6889bcf804d3aa2
AneshkaGoyal <36636940+AneshkaGoyal@users.noreply.github.com>
2023-10-24 14:13:11 +0530

    Update Review.jsx

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -20,7 +20,7 @@ function Review (){
             <Container>
                 <Row>
                     <Col sm={12} md={9} xl={9}>
-                        <h1>What our Patient's Say</h1>
+                        <h1>What our Patients Say</h1>
                     </Col>
                     <Col sm={12} md={3} xl={3}>
                         <a target="_blank" href="https://www.google.com/maps/place/ANESHKA+DENTAL+CARE+CENTRE/@28.4474231,77.0110774,15z/data=!4m8!3m7!1s0x390d182465e660a5:0x3e4afc710f193cbe!8m2!3d28.4443991!4d77.0070797!9m1!1b1!16s%2Fg%2F1tjv1wxz?entry=ttu">Leave us your thoughts</a>
